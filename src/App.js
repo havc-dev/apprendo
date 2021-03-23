f7df3a5b16d8a075e461c8c57cd4bc870c4804f8
Adrián Velázquez <havc.developer@gmail.com>
2021-03-23 04:29:28 -0500

    first commit react base

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Base React File - Anki Apprendemos
+      </h1>
+    </div>
+  );
+}
+
+export default App;
